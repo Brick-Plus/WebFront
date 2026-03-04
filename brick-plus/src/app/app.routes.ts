736fe6ce3ceed242c0a8ff1ea8086dd26b928302
Harcadakad <evan.guillet@ynov.com>
2026-03-04 09:56:24 +0100

    feat: merge setoccasion & setneuf into lego-sets, remove /setoccasion and /setneuf with their routes, add /lego-sets page with unified products and logic

--- brick-plus/src/app/app.routes.ts
+++ brick-plus/src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProfileComponent } from './pages/profile/profile.component';
-import { SetneufComponent } from './pages/setneuf.component/setneuf.component';
-import { SetoccasionComponent } from './pages/setoccasion.component/setoccasion.component';
+import { LegoSetsComponent } from './pages/lego-sets/lego-sets.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
-  { path: 'pages/setneuf', component: SetneufComponent },
-  { path: 'setneuf', component: SetneufComponent },
-  { path: 'setoccasion', component: SetoccasionComponent },
+  { path: 'lego-sets', component: LegoSetsComponent },
 ];
