1dcca076cd93dfce94d0e61736cc8a4f8b2edb2d
salmakacem1999 <125297713+salmakacem1999@users.noreply.github.com>
2026-04-15 13:29:03 +0200

    composant avis partagé + finalisation page set complet

--- brick-plus/src/app/app.routes.ts
+++ brick-plus/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { Produits } from './pages/produits/produits.component';
 import { SetCompletComponent } from './pages/set-complet/set-complet.component';
+import { AvisComponent } from './components/avis/avis.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -12,5 +13,6 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'products', component: Produits },
-  { path: 'complete-set', component: SetCompletComponent }
+  { path: 'complete-set', component: SetCompletComponent },
+  { path: 'avis', component: AvisComponent}
 ];
